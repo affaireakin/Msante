5f4f932dc6a573ba87faa7952a5b8c8167f26c21
M-Sante Dev <dev@m-sante.app>
2026-05-01 23:39:59 +0200

    fix(jest): configure transformIgnorePatterns for pnpm + disable nativewind/reanimated in test env

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,12 +1,15 @@
 module.exports = function (api) {
-  api.cache(true)
+  api.cache(() => process.env.NODE_ENV)
+  const isTest = api.env('test')
   return {
     presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
+      isTest
+        ? 'babel-preset-expo'
+        : ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      ...(!isTest ? ['nativewind/babel'] : []),
     ],
     plugins: [
-      'react-native-reanimated/plugin',
+      ...(!isTest ? ['react-native-reanimated/plugin'] : []),
     ],
   }
 }
